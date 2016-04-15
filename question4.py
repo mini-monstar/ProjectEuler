e30c80a2354d4291a9b2826d780fa9d913c8d269
Jovan <teo.jovan@dhs.sg>
2016-04-15 14:42:07 +0800

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,13 +1,11 @@
 largest = 0
-palindromes = []
+palindrome = 0
 for i in range(999,99,-1):
     for j in range(999,99,-1):
-        if str(i*j)[:3] == str(i*j)[-3:][::-1] and len(str(i*j)) == 6:
-            palindromes.append(i*j)
+        product = str(i*j)
+        if product == product[::-1] and len(str(i*j)) == 6:
+            if int(product) > palindrome:
+                palindrome = int(product)
             break
-
-for i in palindromes:
-    if i > largest:
-        largest = i
         
-print(largest)
+print(palindrome)
